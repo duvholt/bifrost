64b23bb37bcd473f7b8c5a04dcc57b5d18bcb2ab
Christian Iversen <ci@iversenit.dk>
2025-01-12 01:17:40 +0100

    * Refactor option handling in DeviceProductData::guess_from_device() (suggested by clippy)

--- src/hue/api/device.rs
+++ src/hue/api/device.rs
@@ -44,15 +44,15 @@ impl DeviceProductData {
 
     #[must_use]
     pub fn guess_from_device(dev: &z2m::api::Device) -> Self {
-        fn str_or_unknown(name: &Option<String>) -> String {
-            name.clone().unwrap_or_else(|| String::from("<unknown>"))
+        fn str_or_unknown(name: Option<&String>) -> String {
+            name.cloned().unwrap_or_else(|| String::from("<unknown>"))
         }
 
-        let product_name = str_or_unknown(&dev.model_id);
-        let model_id = str_or_unknown(&dev.definition.as_ref().map(|def| def.model.clone()));
-        let manufacturer_name = str_or_unknown(&dev.manufacturer);
+        let product_name = str_or_unknown(dev.model_id.as_ref());
+        let model_id = str_or_unknown(dev.definition.as_ref().map(|def| &def.model));
+        let manufacturer_name = str_or_unknown(dev.manufacturer.as_ref());
         let certified = manufacturer_name == Self::SIGNIFY_MANUFACTURER_NAME;
-        let software_version = str_or_unknown(&dev.software_build_id);
+        let software_version = str_or_unknown(dev.software_build_id.as_ref());
 
         let product_archetype = DeviceArchetype::SpotBulb;
 
