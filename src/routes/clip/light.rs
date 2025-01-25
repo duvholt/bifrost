108bd09a5c11e0c0725915227c8ace36bf9528c8
Christian Iversen <ci@iversenit.dk>
2025-01-26 00:57:31 +0100

    + Implement support for accepting gradient updates in clip route handler

--- src/routes/clip/light.rs
+++ src/routes/clip/light.rs
@@ -22,17 +22,29 @@ async fn put_light(
     log::debug!("json data\n{}", serde_json::to_string_pretty(&put)?);
 
     let rlink = RType::Light.link_to(id);
-    let lock = state.res.lock().await;
+    let mut lock = state.res.lock().await;
 
     let _ = lock.get::<Light>(&rlink)?;
 
     let upd: LightUpdate = serde_json::from_value(put)?;
 
+    // We cannot recover .mode from backend updates, since these only contain
+    // the gradiant colors. So we have no choice, but to update the mode
+    // here. Otherwise, the information would be lost.
+    if let Some(mode) = upd.gradient.as_ref().and_then(|gr| gr.mode) {
+        lock.update::<Light>(&rlink.rid, |light| {
+            if let Some(gr) = &mut light.gradient {
+                gr.mode = mode;
+            }
+        })?;
+    }
+
     let payload = DeviceUpdate::default()
         .with_state(upd.on.map(|on| on.on))
         .with_brightness(upd.dimming.map(|dim| dim.brightness / 100.0 * 254.0))
         .with_color_temp(upd.color_temperature.map(|ct| ct.mirek))
-        .with_color_xy(upd.color.map(|col| col.xy));
+        .with_color_xy(upd.color.map(|col| col.xy))
+        .with_gradient(upd.gradient);
 
     lock.z2m_request(ClientRequest::light_update(rlink, payload))?;
 
