771d9355cbe3145d729c3838729139863ff92343
Christian Iversen <ci@iversenit.dk>
2025-02-09 00:33:23 +0100

    * Move lint and clippy settings to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
-#![warn(
-    clippy::all,
-    clippy::correctness,
-    clippy::pedantic,
-    clippy::cargo,
-    clippy::nursery,
-    clippy::perf,
-    clippy::style
-)]
-#![allow(
-    clippy::multiple_crate_versions,
-    clippy::missing_errors_doc,
-    clippy::missing_panics_doc,
-    clippy::module_name_repetitions
-)]
-
 pub mod backend;
 pub mod config;
 pub mod error;
