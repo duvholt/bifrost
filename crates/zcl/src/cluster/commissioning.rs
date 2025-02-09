138fa06fc4fb2ad5bf530f3f74b0354afe24d0bc
Christian Iversen <ci@iversenit.dk>
2025-02-09 12:35:38 +0100

    * Make Commissioning dissector support ScanRequest

--- crates/zcl/src/cluster/commissioning.rs
+++ crates/zcl/src/cluster/commissioning.rs
@@ -3,12 +3,18 @@ use crate::frame::ZclFrame;
 use hue::zigbee::HueEntFrame;
 
 pub fn describe(frame: &ZclFrame, data: &[u8]) -> ZclResult<Option<String>> {
-    if frame.cluster_specific() && frame.cmd == 0x02 {
-        let (data, csum) = data.split_at(data.len() - 4);
-        let csum = u32::from_be_bytes([csum[0], csum[1], csum[2], csum[3]]);
-        let hes = HueEntFrame::parse(data)?;
-        Ok(Some(format!("{hes:x?} [PROXY, {csum:08x}]")))
-    } else {
-        Ok(None)
+    if !frame.cluster_specific() {
+        return Ok(None);
+    }
+
+    match frame.cmd {
+        0x00 => Ok(Some("ScanRequest".to_string())),
+        0x02 => {
+            let (data, csum) = data.split_at(data.len() - 4);
+            let csum = u32::from_be_bytes([csum[0], csum[1], csum[2], csum[3]]);
+            let hes = HueEntFrame::parse(data)?;
+            Ok(Some(format!("{hes:x?} [PROXY, {csum:08x}]")))
+        }
+        _ => Ok(None),
     }
 }
