26b60764d7203b5cac7bbdc6f2f507d7d58d15a2
Christian Iversen <ci@iversenit.dk>
2025-02-18 23:05:56 +0100

    + Add data models for hue entertainment network streams (over DTLS on port 2100)

---
+++ crates/hue/src/stream.rs
@@ -0,0 +1,50 @@
+use packed_struct::prelude::*;
+use uuid::Uuid;
+
+use crate::error::HueResult;
+
+#[derive(PrimitiveEnum_u8, Clone, Debug, Copy)]
+pub enum HueStreamColorMode {
+    Rgb = 0x00,
+    Xy = 0x01,
+}
+
+#[derive(PackedStruct, Clone, Debug)]
+#[packed_struct(size = "52", endian = "msb")]
+pub struct HueStreamHeader {
+    magic: [u8; 9],
+    version: u16,
+    seqnr: u8,
+    x0: u16,
+    #[packed_field(size_bytes = "1", ty = "enum")]
+    color_mode: HueStreamColorMode,
+    x1: u8,
+    dest: [u8; 36],
+}
+
+#[derive(Clone, Debug)]
+pub struct HueStreamPacket {
+    pub color_mode: HueStreamColorMode,
+    pub area: Uuid,
+}
+
+impl HueStreamPacket {
+    pub fn parse(data: &[u8]) -> HueResult<Self> {
+        let hdr =
+            HueStreamHeader::unpack_from_slice(&data[..HueStreamHeader::packed_bytes_size(None)?])?;
+        debug_assert_eq!(&hdr.magic, b"HueStream");
+        Ok(Self {
+            color_mode: hdr.color_mode,
+            area: Uuid::try_parse_ascii(&hdr.dest)?,
+        })
+    }
+}
+
+#[derive(PackedStruct, Clone, Debug, Copy)]
+#[packed_struct(size = "7", endian = "msb")]
+pub struct HueStreamLight {
+    pub channel: u8,
+    pub r: u16,
+    pub g: u16,
+    pub b: u16,
+}
