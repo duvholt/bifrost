e75a5455b4603aeb865764b5700b873c1a587161
Christian Iversen <ci@iversenit.dk>
2025-02-23 14:27:37 +0100

    + Convert some of the matrix code from Thomas Lochmatter, and refactor color spaces to use new Matrix3 struct

--- crates/hue/src/colorspace.rs
+++ crates/hue/src/colorspace.rs
@@ -6,25 +6,101 @@
 
 use crate::gamma::GammaCorrection;
 
-pub struct ColorSpace {
-    rgb: [f64; 9],
-    xyz: [f64; 9],
-    gamma: GammaCorrection,
-}
+#[derive(Clone, Debug)]
+pub struct Matrix3(pub [f64; 3 * 3]);
 
-impl ColorSpace {
+impl Matrix3 {
+    pub const fn identity() -> Self {
+        Self([
+            1.0, 0.0, 0.0, //
+            0.0, 1.0, 0.0, //
+            0.0, 0.0, 1.0, //
+        ])
+    }
+
+    pub fn inverted(&self) -> Option<Self> {
+        let mut current = self.clone();
+        let mut inverse = Self::identity();
+
+        // Gaussian elimination (part 1)
+        for i in 0..3 {
+            // Get the diagonal term
+            let mut d = current.0[i * 3 + i];
+
+            // If it is 0, there must be at least one row with a non-zero element (otherwise, the matrix is not invertible)
+            if d == 0.0 {
+                let mut r = i + 1;
+
+                while r < 3 && (current.0[r * 3 + i]).abs() < 1e-10 {
+                    r += 1
+                }
+
+                if r == 3 {
+                    return None;
+                } // i is the rank
+
+                for c in 0..3 {
+                    current.0[i * 3 + c] += current.0[r * 3 + c];
+                    inverse.0[i * 3 + c] += inverse.0[r * 3 + c];
+                }
+
+                d = current.0[i * 3 + i];
+            }
+
+            // Divide the row by the diagonal term
+            let inv = 1.0 / d;
+            for c in 0..3 {
+                current.0[i * 3 + c] *= inv;
+                inverse.0[i * 3 + c] *= inv;
+            }
+
+            // Divide all subsequent rows with a non-zero coefficient, and subtract the row
+            for r in i + 1..3 {
+                let p = current.0[r * 3 + i];
+                if p != 0.0 {
+                    for c in 0..3 {
+                        current.0[r * 3 + c] -= current.0[i * 3 + c] * p;
+                        inverse.0[r * 3 + c] -= inverse.0[i * 3 + c] * p;
+                    }
+                }
+            }
+        }
+
+        // Gaussian elimination (part 2)
+        for i in (0..3).rev() {
+            for r in 0..i {
+                let d = current.0[r * 3 + i];
+                for c in 0..3 {
+                    current.0[r * 3 + c] -= current.0[i * 3 + c] * d;
+                    inverse.0[r * 3 + c] -= inverse.0[i * 3 + c] * d;
+                }
+            }
+        }
+
+        Some(inverse)
+    }
+
     #[allow(clippy::suboptimal_flops)]
     #[must_use]
-    fn mult(d: [f64; 3], m: &[f64; 9]) -> [f64; 3] {
+    fn mult(&self, d: [f64; 3]) -> [f64; 3] {
+        let m = self.0;
         let cx = d[0] * m[0] + d[1] * m[1] + d[2] * m[2];
         let cy = d[0] * m[3] + d[1] * m[4] + d[2] * m[5];
         let cz = d[0] * m[6] + d[1] * m[7] + d[2] * m[8];
         [cx, cy, cz]
     }
+}
 
+pub struct ColorSpace {
+    rgb: Matrix3,
+    xyz: Matrix3,
+    gamma: GammaCorrection,
+}
+
+impl ColorSpace {
     #[must_use]
     pub fn xyz_to_rgb(&self, x: f64, y: f64, z: f64) -> [f64; 3] {
-        Self::mult([x, y, z], &self.rgb).map(|q| self.gamma.transform(q))
+        self.rgb.mult([x, y, z]).map(|q| self.gamma.transform(q))
     }
 
     #[allow(non_snake_case)]
@@ -36,7 +112,7 @@ impl ColorSpace {
 
     #[must_use]
     pub fn rgb_to_xyz(&self, r: f64, g: f64, b: f64) -> [f64; 3] {
-        Self::mult([r, g, b].map(|q| self.gamma.inverse(q)), &self.xyz)
+        self.xyz.mult([r, g, b].map(|q| self.gamma.inverse(q)))
     }
 
     #[allow(clippy::many_single_char_names)]
@@ -74,45 +150,45 @@ impl ColorSpace {
 
 /// Wide gamut color space
 pub const WIDE: ColorSpace = ColorSpace {
-    rgb: [
+    rgb: Matrix3([
         1.4625, -0.1845, -0.2734, //
         -0.5228, 1.4479, 0.0681, //
         0.0346, -0.0958, 1.2875, //
-    ],
-    xyz: [
+    ]),
+    xyz: Matrix3([
         0.7164, 0.1010, 0.1468, //
         0.2587, 0.7247, 0.0166, //
         0.0000, 0.0512, 0.7740, //
-    ],
+    ]),
     gamma: GammaCorrection::NONE,
 };
 
 /// sRGB color space
 pub const SRGB: ColorSpace = ColorSpace {
-    rgb: [
+    rgb: Matrix3([
         3.2405, -1.5371, -0.4985, //
         -0.9693, 1.8760, 0.0416, //
         0.0556, -0.2040, 1.0572, //
-    ],
-    xyz: [
+    ]),
+    xyz: Matrix3([
         0.4125, 0.3576, 0.1804, //
         0.2127, 0.7152, 0.0722, //
         0.0193, 0.1192, 0.9503, //
-    ],
+    ]),
     gamma: GammaCorrection::SRGB,
 };
 
 /// Adobe RGB color space
 pub const ADOBE: ColorSpace = ColorSpace {
-    rgb: [
+    rgb: Matrix3([
         2.0416, -0.5652, -0.3447, //
         -0.9695, 1.8763, 0.0415, //
         0.0135, -0.1184, 1.0154, //
-    ],
-    xyz: [
+    ]),
+    xyz: Matrix3([
         0.5767, 0.1856, 0.1882, //
         0.2974, 0.6273, 0.0753, //
         0.0270, 0.0707, 0.9911, //
-    ],
+    ]),
     gamma: GammaCorrection::NONE,
 };
