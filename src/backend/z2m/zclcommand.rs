ae8347cd92b430076e04e8a6769df1b75686e5e1
Christian Iversen <ci@iversenit.dk>
2025-02-26 00:54:20 +0100

    * Update hue_zclcommand() for new support built into newest z2m versions

--- src/backend/z2m/zclcommand.rs
+++ src/backend/z2m/zclcommand.rs
@@ -4,18 +4,19 @@ use crate::z2m::api::RawMessage;
 
 pub const PHILIPS_HUE_ZIGBEE_VENDOR_ID: u16 = 0x100B;
 
-/// Custom, experimental extension for `Zigbee2MQTT`, which allows entirely
-/// free-form zigbee messages to be sent.
+/// Use the low-level endpoint for `Zigbee2MQTT`, which allows free-form zigbee
+/// messages to be sent.
 ///
-/// NOTE: The generated z2m payload only works on patches z2m instances.
+/// NOTE: The generated z2m payload only works on z2m instances with
+/// Zigbee-Herdsman-Converter version 23.0.0 or greater.
 ///
-/// A REGULAR INSTALL OF Z2M WILL NOT WORK.
+/// OLDER VERSIONS WILL NOT WORK.
 #[must_use]
 pub fn hue_zclcommand(topic: &str, cluster: &str, command: u8, data: &[u8]) -> RawMessage {
     RawMessage {
         topic: topic.to_string(),
         payload: json!({
-            "zclcommand2": {
+            "zclcommand": {
                 "cluster": cluster,
                 "command": command,
                 "payload": {
