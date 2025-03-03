666d6da982920acafa6f22ca333d1dee0dca005d
Christian Iversen <ci@iversenit.dk>
2025-03-03 20:27:22 +0100

    - Remove old obsolete http/https service impls

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -7,7 +7,6 @@ pub mod updater;
 
 use std::fs::File;
 use std::io::Write;
-use std::net::{Ipv4Addr, SocketAddr};
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -16,10 +15,8 @@ use axum::extract::Request;
 use axum::response::Response;
 use axum::routing::IntoMakeService;
 use axum::{Router, ServiceExt};
-use axum_server::service::MakeService;
 
 use camino::Utf8PathBuf;
-use hyper::body::Incoming;
 use tokio::select;
 use tokio::sync::Mutex;
 use tokio::time::{sleep_until, MissedTickBehavior};
@@ -63,91 +60,6 @@ pub fn build_service(appstate: AppState) -> IntoMakeService<NormalizePath<Router
     let normalized = NormalizePathLayer::trim_trailing_slash().layer(router(appstate));
 
     ServiceExt::<Request>::into_make_service(normalized)
-}
-
-pub async fn http_server<S>(listen_addr: Ipv4Addr, listen_port: u16, svc: S) -> ApiResult<()>
-where
-    S: Send + MakeService<SocketAddr, Request<Incoming>>,
-    S::MakeFuture: Send,
-{
-    let addr = SocketAddr::from((listen_addr, listen_port));
-    log::info!("http listening on {}", addr);
-
-    axum_server::bind(addr).serve(svc).await?;
-
-    Ok(())
-}
-
-#[cfg(feature = "tls-openssl")]
-pub async fn https_server_openssl<S>(
-    listen_addr: Ipv4Addr,
-    listen_port: u16,
-    svc: S,
-    certfile: Utf8PathBuf,
-) -> ApiResult<()>
-where
-    S: Send + MakeService<SocketAddr, Request<Incoming>>,
-    S::MakeFuture: Send,
-{
-    use axum_server::tls_openssl::OpenSSLConfig;
-    use openssl::ssl::{AlpnError, SslAcceptor, SslFiletype, SslMethod, SslRef};
-
-    fn alpn_select<'a>(_tls: &mut SslRef, client: &'a [u8]) -> Result<&'a [u8], AlpnError> {
-        openssl::ssl::select_next_proto(b"\x02h2\x08http/1.1", client).ok_or(AlpnError::NOACK)
-    }
-
-    // the default axum-server function for configuring openssl uses
-    // [`SslAcceptor::mozilla_modern_v5`], which requires TLSv1.3.
-    //
-    // That protocol version is too new for some important clients, like
-    // Hue Sync for PC, so manually construct an OpenSSLConfig here, with
-    // slightly more relaxed settings.
-
-    log::debug!("Loading certificate from [{certfile}]");
-
-    let mut tls_builder = SslAcceptor::mozilla_intermediate_v5(SslMethod::tls())?;
-    tls_builder.set_certificate_file(&certfile, SslFiletype::PEM)?;
-    tls_builder.set_private_key_file(&certfile, SslFiletype::PEM)?;
-    tls_builder.check_private_key()?;
-    tls_builder.set_alpn_select_callback(alpn_select);
-    let acceptor = tls_builder.build();
-
-    let config = OpenSSLConfig::from_acceptor(Arc::new(acceptor));
-
-    let addr = SocketAddr::from((listen_addr, listen_port));
-
-    log::info!("https listening on {}", addr);
-    axum_server::bind_openssl(addr, config).serve(svc).await?;
-
-    Ok(())
-}
-
-#[cfg(feature = "tls-rustls")]
-pub async fn https_server_rustls<S>(
-    listen_addr: Ipv4Addr,
-    listen_port: u16,
-    svc: S,
-    certfile: Utf8PathBuf,
-) -> ApiResult<()>
-where
-    S: Send + MakeService<SocketAddr, Request<Incoming>>,
-    S::MakeFuture: Send,
-{
-    use crate::error::ApiError;
-    use axum_server::tls_rustls::RustlsConfig;
-
-    log::debug!("Loading certificate from [{certfile}]");
-
-    let config = RustlsConfig::from_pem_file(&certfile, &certfile)
-        .await
-        .map_err(|e| ApiError::Certificate(certfile.clone(), e))?;
-
-    let addr = SocketAddr::from((listen_addr, listen_port));
-
-    log::info!("https listening on {}", addr);
-    axum_server::bind_rustls(addr, config).serve(svc).await?;
-
-    Ok(())
 }
 
 pub async fn config_writer(res: Arc<Mutex<Resources>>, filename: Utf8PathBuf) -> ApiResult<()> {
