98860f91cfd0ac8d8a649827697378c6c7dc206b
Christian Iversen <ci@iversenit.dk>
2025-03-04 23:06:31 +0100

    * Rework EntertainmentZigbeeStream by no longer embedding a ZigbeeTarget, and spin out ZigbeeMessage struct

--- crates/hue/src/zigbee/stream.rs
+++ crates/hue/src/zigbee/stream.rs
@@ -1,20 +1,45 @@
 use packed_struct::prelude::*;
 
 use crate::error::HueResult;
-use crate::zigbee::{HueEntFrame, HueEntFrameLightRecord, HueEntStop, ZigbeeTarget};
+use crate::zigbee::{HueEntFrame, HueEntFrameLightRecord, HueEntSegmentConfig, HueEntStop};
 
-pub struct EntertainmentZigbeeStream<T: ZigbeeTarget> {
-    pub target: T,
+pub struct EntertainmentZigbeeStream {
     smoothing: u16,
     counter: u32,
 }
 
-impl<T: ZigbeeTarget> EntertainmentZigbeeStream<T> {
-    pub const DEFAULT_SMOOTHING: u16 = 0x0400;
+#[derive(Debug, Clone)]
+pub struct ZigbeeMessage {
+    pub cluster: u16,
+    pub command: u8,
+    pub data: Vec<u8>,
+}
 
-    pub fn new(target: T) -> Self {
+impl ZigbeeMessage {
+    pub fn new(cluster: u16, command: u8, data: Vec<u8>) -> Self {
         Self {
-            target,
+            cluster,
+            command,
+            data,
+        }
+    }
+}
+
+impl Default for EntertainmentZigbeeStream {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl EntertainmentZigbeeStream {
+    pub const DEFAULT_SMOOTHING: u16 = 0x0400;
+    pub const CLUSTER: u16 = 0xFC01;
+    pub const CMD_SEGMENT_MAP: u8 = 7;
+    pub const CMD_RESET: u8 = 3;
+    pub const CMD_FRAME: u8 = 1;
+
+    pub const fn new() -> Self {
+        Self {
             smoothing: Self::DEFAULT_SMOOTHING,
             counter: 0,
         }
@@ -28,7 +53,17 @@ impl<T: ZigbeeTarget> EntertainmentZigbeeStream<T> {
         self.smoothing = smoothing;
     }
 
-    pub fn reset(&mut self) -> HueResult<T::Result> {
+    pub fn segment_mapping(&mut self, map: &[u16]) -> HueResult<ZigbeeMessage> {
+        let msg = HueEntSegmentConfig::new(map);
+
+        Ok(ZigbeeMessage::new(
+            Self::CLUSTER,
+            Self::CMD_SEGMENT_MAP,
+            msg.pack()?,
+        ))
+    }
+
+    pub fn reset(&mut self) -> HueResult<ZigbeeMessage> {
         let ent = HueEntStop {
             x0: 0,
             x1: 1,
@@ -37,10 +72,14 @@ impl<T: ZigbeeTarget> EntertainmentZigbeeStream<T> {
 
         self.counter += 1;
 
-        self.target.send(0xFC01, 3, &ent.pack()?)
+        Ok(ZigbeeMessage::new(
+            Self::CLUSTER,
+            Self::CMD_RESET,
+            ent.pack_to_vec()?,
+        ))
     }
 
-    pub fn frame(&mut self, blks: Vec<HueEntFrameLightRecord>) -> HueResult<T::Result> {
+    pub fn frame(&mut self, blks: Vec<HueEntFrameLightRecord>) -> HueResult<ZigbeeMessage> {
         let ent = HueEntFrame {
             counter: self.counter,
             smoothing: self.smoothing,
@@ -49,6 +88,10 @@ impl<T: ZigbeeTarget> EntertainmentZigbeeStream<T> {
 
         self.counter += 1;
 
-        self.target.send(0xFC01, 1, &ent.pack()?)
+        Ok(ZigbeeMessage::new(
+            Self::CLUSTER,
+            Self::CMD_FRAME,
+            ent.pack()?,
+        ))
     }
 }
