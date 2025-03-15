aa78fcfb91c2c97729339db599143e9ceb41210d
Christian Iversen <ci@iversenit.dk>
2025-03-15 16:26:12 +0100

    + Implement support for upnp-related "/description.xml" endpoint.
    This endpoint helps some clients find Bifrost on the network.
    
    For example, the android app "hueDynamic" seems to require this endpoint, but
    it's likely that other programs will benefit as well.
    
    The output has been carefully reviewed to match the original hue bridge output
    in syntax, order and meaning. The only differences are formatting changes
    (indentation is enabled for Bifrost), and the values of the various xml fields.

---
+++ src/routes/upnp.rs
@@ -0,0 +1,43 @@
+use axum::extract::State;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::Router;
+use hyper::header::CONTENT_TYPE;
+use hyper::HeaderMap;
+use url::Url;
+use uuid::Uuid;
+
+use crate::error::ApiResult;
+use crate::model::upnp;
+use crate::server::appstate::AppState;
+
+async fn description_xml(State(state): State<AppState>) -> ApiResult<impl IntoResponse> {
+    let mac = state.api_short_config().await.mac;
+    let config = &state.config().bridge;
+    let ip = config.ipaddress;
+    let port = config.http_port;
+
+    let url_base = Url::parse(&format!("http://{ip}:{port}/"))?;
+    let friendly_name = format!("Bifrost {ip}");
+    let manufacturer = "Christian Iversen";
+    let model_name = "Bifrost Bridge";
+    let udn = Uuid::new_v5(&Uuid::NAMESPACE_OID, &mac.bytes());
+
+    let device = upnp::Device::new(friendly_name, manufacturer, model_name, udn)
+        .with_manufacturer_url(Url::parse("http://github.com/chrivers/bifrost")?)
+        .with_model_description("Bifrost Hue Bridge Emulator")
+        .with_model_number(hue::HUE_BRIDGE_V2_MODEL_ID)
+        .with_model_url(Url::parse("http://www.philips-hue.com")?)
+        .with_serial_number(hex::encode(mac.bytes()))
+        .with_presentation_url("index.html");
+
+    let root = upnp::Root::new(url_base, device);
+
+    let mut headers = HeaderMap::new();
+    headers.insert(CONTENT_TYPE, "text/xml".parse().unwrap());
+    Ok((headers, upnp::to_xml(&root)?))
+}
+
+pub fn router() -> Router<AppState> {
+    Router::new().route("/", get(description_xml))
+}
