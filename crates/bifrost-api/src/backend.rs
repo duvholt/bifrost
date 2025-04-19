9e671488e45734694c2a76dc6058126fa321e0a8
Christian Iversen <ci@iversenit.dk>
2025-04-19 21:50:55 +0200

    + Add BackendRequest to bifrost-api (copied from bifrost, will be removed from there)

--- crates/bifrost-api/src/backend.rs
+++ crates/bifrost-api/src/backend.rs
@@ -1,6 +1,31 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+use hue::api::{GroupedLightUpdate, LightUpdate, ResourceLink, RoomUpdate, Scene, SceneUpdate};
+use hue::stream::HueStreamLights;
+
 use crate::Client;
 use crate::config::Z2mServer;
 use crate::error::BifrostResult;
+
+#[allow(clippy::large_enum_variant)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum BackendRequest {
+    LightUpdate(ResourceLink, LightUpdate),
+
+    SceneCreate(ResourceLink, u32, Scene),
+    SceneUpdate(ResourceLink, SceneUpdate),
+
+    GroupedLightUpdate(ResourceLink, GroupedLightUpdate),
+
+    RoomUpdate(ResourceLink, RoomUpdate),
+
+    Delete(ResourceLink),
+
+    EntertainmentStart(Uuid),
+    EntertainmentFrame(HueStreamLights),
+    EntertainmentStop(),
+}
 
 impl Client {
     pub async fn post_backend(&self, name: &str, backend: Z2mServer) -> BifrostResult<()> {
