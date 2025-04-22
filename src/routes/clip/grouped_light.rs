dfb8b0c1c7373de07e0811ccf6c7848a471196c6
Christian Iversen <ci@iversenit.dk>
2025-04-22 21:20:34 +0200

    - Remove obsolete code from routes::clip::grouped_light

--- src/routes/clip/grouped_light.rs
+++ src/routes/clip/grouped_light.rs
@@ -1,13 +1,10 @@
 use axum::extract::{Path, State};
-use axum::routing::{get, put};
-use axum::Router;
 use serde_json::Value;
 use uuid::Uuid;
 
 use hue::api::{GroupedLight, GroupedLightUpdate, RType};
 
 use crate::backend::BackendRequest;
-use crate::routes::clip::generic::get_resource;
 use crate::routes::clip::{ApiV2Result, V2Reply};
 use crate::routes::extractor::Json;
 use crate::server::appstate::AppState;
@@ -33,21 +30,4 @@ pub async fn put_grouped_light(
     drop(lock);
 
     V2Reply::ok(rlink)
-}
-
-async fn get_grouped_light(State(state): State<AppState>, Path(id): Path<Uuid>) -> ApiV2Result {
-    V2Reply::ok(
-        state
-            .res
-            .lock()
-            .await
-            .get_resource(RType::GroupedLight, &id)?,
-    )
-}
-
-pub fn router() -> Router<AppState> {
-    Router::new()
-        .route("/", get(|st| get_resource(st, Path(RType::GroupedLight))))
-        .route("/{id}", get(get_grouped_light))
-        .route("/{id}", put(put_grouped_light))
 }
