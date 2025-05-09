386efd515dc1ebc437fe15dba5510b9a771d3e90
Christian Iversen <ci@iversenit.dk>
2025-05-09 17:10:10 +0200

    + Add Z2mWebSocket::send_device_remove()

--- src/backend/z2m/websocket.rs
+++ src/backend/z2m/websocket.rs
@@ -6,7 +6,7 @@ use hue::zigbee::{HueZigbeeUpdate, ZigbeeMessage};
 use tokio::net::TcpStream;
 use tokio_tungstenite::tungstenite::{self, Message};
 use tokio_tungstenite::{MaybeTlsStream, WebSocketStream};
-use z2m::api::{GroupMemberChange, PermitJoin};
+use z2m::api::{DeviceRemove, GroupMemberChange, PermitJoin};
 use z2m::request::Z2mPayload;
 use z2m::update::DeviceUpdate;
 use z2m::{api::RawMessage, request::Z2mRequest};
@@ -50,6 +50,10 @@ impl Z2mWebSocket {
             Z2mRequest::PermitJoin(value) => RawMessage {
                 topic: "bridge/request/permit_join".into(),
                 payload: serde_json::to_value(value)?,
+            },
+            Z2mRequest::DeviceRemove(dev) => RawMessage {
+                topic: "bridge/request/device/remove".into(),
+                payload: serde_json::to_value(dev)?,
             },
             _ => RawMessage {
                 topic: format!("{topic}/set"),
@@ -153,6 +157,12 @@ impl Z2mWebSocket {
 
     pub async fn send_permit_join(&mut self, time: u32, device: Option<String>) -> ApiResult<()> {
         let z2mreq = Z2mRequest::PermitJoin(PermitJoin { time, device });
+
+        self.send("", &z2mreq).await
+    }
+
+    pub async fn send_device_remove(&mut self, id: String) -> ApiResult<()> {
+        let z2mreq = Z2mRequest::DeviceRemove(DeviceRemove { id });
 
         self.send("", &z2mreq).await
     }
