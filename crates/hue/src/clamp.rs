671560b2f45e03db30c1dd683dff93fe019f1720
Christian Iversen <ci@iversenit.dk>
2025-05-21 18:11:22 +0200

    * Make Clamp::unit_to_u8_clamped() perform proper rounding. (found by unit testing)

--- crates/hue/src/clamp.rs
+++ crates/hue/src/clamp.rs
@@ -23,7 +23,7 @@ impl Clamp for f32 {
 impl Clamp for f64 {
     #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
     fn unit_to_u8_clamped(self) -> u8 {
-        (self * 255.0).clamp(0.0, 255.0) as u8
+        (self * 255.0).round().clamp(0.0, 255.0) as u8
     }
 
     #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
