5c80e9ababe01567656b03edecbc7a544ef6bc37
Christian Iversen <ci@iversenit.dk>
2025-05-22 16:28:15 +0200

    + Add missing .round() on Clamp impls (found by unit testing)

--- crates/hue/src/clamp.rs
+++ crates/hue/src/clamp.rs
@@ -7,12 +7,12 @@ pub trait Clamp {
 impl Clamp for f32 {
     #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
     fn unit_to_u8_clamped(self) -> u8 {
-        (self * 255.0).clamp(0.0, 255.0) as u8
+        (self * 255.0).round().clamp(0.0, 255.0) as u8
     }
 
     #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
     fn unit_to_u8_clamped_light(self) -> u8 {
-        self.mul_add(253.0, 1.0).clamp(1.0, 254.0) as u8
+        self.mul_add(253.0, 1.0).round().clamp(1.0, 254.0) as u8
     }
 
     fn unit_from_u8(value: u8) -> Self {
@@ -28,7 +28,7 @@ impl Clamp for f64 {
 
     #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
     fn unit_to_u8_clamped_light(self) -> u8 {
-        self.mul_add(253.0, 1.0).clamp(1.0, 254.0) as u8
+        self.mul_add(253.0, 1.0).round().clamp(1.0, 254.0) as u8
     }
 
     fn unit_from_u8(value: u8) -> Self {
