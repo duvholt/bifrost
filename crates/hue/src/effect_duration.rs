badaf128b43849c51e2626dfbaa4e22f6c697dde
Christian Duvholt <cxhristian@gmail.com>
2025-05-25 16:23:14 +0200

    Change last effect duration step to 60min
    This is still just a guess but it makes sense with the minimal sampling we have

--- crates/hue/src/effect_duration.rs
+++ crates/hue/src/effect_duration.rs
@@ -1,4 +1,4 @@
-use crate::{error::HueResult};
+use crate::error::HueResult;
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct EffectDuration(pub u8);
@@ -7,19 +7,20 @@ const RESOLUTION_01S_BASE: u8 = 0xFC;
 const RESOLUTION_05S_BASE: u8 = 0xCC;
 const RESOLUTION_15S_BASE: u8 = 0xA5;
 const RESOLUTION_01M_BASE: u8 = 0x79;
-const RESOLUTION_05M_BASE: u8 = 0x3F;
+const RESOLUTION_60M_BASE: u8 = 0x3F;
 
-const RESOLUTION_01S: u32 = 1; // 01s.
-const RESOLUTION_05S: u32 = 5; // 05s.
+const RESOLUTION_01S: u32 = 1; // 1s.
+const RESOLUTION_05S: u32 = 5; // 5s.
 const RESOLUTION_15S: u32 = 15; // 15s.
-const RESOLUTION_01M: u32 = 60; // 01min.
-const RESOLUTION_05M: u32 = 5 * 600; // 05min.
+const RESOLUTION_01M: u32 = 60; // 1min.
+// This value is just a guess. More real world testing is required
+const RESOLUTION_60M: u32 = 60 * 60; // 60min.
 
 const RESOLUTION_01S_LIMIT: u32 = 60; // 01min.
 const RESOLUTION_05S_LIMIT: u32 = 5 * 60; // 05min.
 const RESOLUTION_15S_LIMIT: u32 = 15 * 60; // 15min.
 const RESOLUTION_01M_LIMIT: u32 = 60 * 60; // 60min.
-const RESOLUTION_05M_LIMIT: u32 = 6 * 60 * 60; // 06hrs.
+const RESOLUTION_60M_LIMIT: u32 = 6 * 60 * 60; // 06hrs.
 
 impl EffectDuration {
     #[allow(clippy::cast_possible_truncation)]
@@ -32,8 +33,8 @@ impl EffectDuration {
             (RESOLUTION_15S_BASE, RESOLUTION_15S)
         } else if seconds < RESOLUTION_01M_LIMIT {
             (RESOLUTION_01M_BASE, RESOLUTION_01M)
-        } else if seconds < RESOLUTION_05M_LIMIT {
-            (RESOLUTION_05M_BASE, RESOLUTION_05M)
+        } else if seconds < RESOLUTION_60M_LIMIT {
+            (RESOLUTION_60M_BASE, RESOLUTION_60M)
         } else {
             return Err(crate::error::HueError::EffectDurationOutOfRange(seconds));
         };
@@ -67,6 +68,21 @@ mod tests {
                 EffectDuration::from_seconds(input * 60).unwrap(),
                 EffectDuration(output)
             );
+        }
+    }
+
+    #[test]
+    pub fn check_for_gaps() {
+        // this test only verifies that there are no gaps when converting from seconds to effect duration
+        // the steps and resolution might still be wrong
+        let six_hours = 6 * 60 * 60;
+        let mut prev = 253;
+        for seconds in 0..six_hours {
+            let EffectDuration(next) = EffectDuration::from_seconds(seconds).unwrap();
+            if next != prev {
+                assert_eq!(next, prev - 1, "Skipped at {seconds}s");
+                prev = next;
+            }
         }
     }
 
