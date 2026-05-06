23f8b43a3e64e4e2c423a316eb6095b020d0777a
Cody <intecpsp@gmail.com>
2026-05-06 12:38:15 -0400

    fix: Add SSE KeepAlive to prevent Hue Sync Box idle timeouts

--- src/routes/eventstream.rs
+++ src/routes/eventstream.rs
@@ -1,10 +1,11 @@
 use axum::Router;
 use axum::extract::State;
 use axum::http::{HeaderMap, HeaderValue};
-use axum::response::sse::{Event, Sse};
+use axum::response::sse::{Event, KeepAlive, Sse};
 use axum::routing::get;
 use futures::StreamExt;
 use futures::stream::{self, Stream};
+use std::time::Duration;
 use tokio_stream::wrappers::BroadcastStream;
 
 use crate::error::ApiResult;
@@ -46,6 +47,7 @@ pub async fn get_clip_v2(
     });
 
     Sse::new(hello.chain(stream))
+        .keep_alive(KeepAlive::new().interval(Duration::from_secs(15)).text("hi"))
 }
 
 pub fn router() -> Router<AppState> {
